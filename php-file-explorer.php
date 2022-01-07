983bd45ad93d654db88b890d4162b5bed8a3af16
JaxonRailey <96697458+JaxonRailey@users.noreply.github.com>
2022-01-07 19:03:44 +0100

    Changed browser tab title

--- php-file-explorer.php
+++ php-file-explorer.php
@@ -37,7 +37,7 @@ if (is_array($items) && count($items)) {
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Directory</title>
+        <title>PHP File Explorer</title>
 
         <style>
 
